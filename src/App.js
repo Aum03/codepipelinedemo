b5750af967666c87fbbde30dc0665206d31acbb1
Aum03 <aumyadav0311@gmail.com>
2023-09-02 12:58:02 +0530

    Version-2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello,Testing CI/CD
+         Hello world version 2
         </p>
         
       </header>
