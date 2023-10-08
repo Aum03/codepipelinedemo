5b9d61e540b63c4dc04b3f2c014a375ae45c5797
Aum <aumyadav0311@gmail.com>
2023-10-08 18:40:05 +0530

    modified before install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,10 +7,10 @@ cd /home/ec2-user/server
 
 # For Debian/Ubuntu-based systems
   sudo yum install https://rpm.nodesource.com/pub_18.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
-    sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
+    sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1 --skip-broken
 #_Download_Node_&NPM
 
-yum -y install nodejs npm
+yum -y install  npm
 
 #_Download_PM2
 npm install pm2@latest -g
