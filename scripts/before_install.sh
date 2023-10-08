8e82e4b40fdd19a75543ee8bc121a0a032ea76c0
Aum <aumyadav0311@gmail.com>
2023-10-08 14:33:42 +0530

    Second commit

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,11 +3,37 @@
 #_Change_Working_Directory
 cd /home/ec2-user/server
 
-#_Update_&_Set_Node_Version
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+#_Install_Development_Tools
+yum groupinstall -y "Development Tools"
 
-#_Download_Node_&NPM
-yum -y install nodejs npm
+#_Install_Required_Packages
+yum install -y gcc-c++ make
+
+#_Download_Node.js_Source_Code
+curl -o node-v14.18.0.tar.gz https://nodejs.org/dist/v14.18.0/node-v14.18.0.tar.gz
+
+#_Extract_Node.js_Source_Code
+tar -zxvf node-v14.18.0.tar.gz
+
+#_Change_to_Node.js_Source_Code_Directory
+cd node-v14.18.0
+
+#_Configure_Node.js_Build
+./configure
+
+#_Compile_and_Install_Node.js
+make
+sudo make install
+
+#_Clean_Up
+cd ..
+rm -rf node-v14.18.0.tar.gz node-v14.18.0
+
+#_Install_NPM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bash_profile
+nvm install 14.18.0
+nvm use 14.18.0
 
 #_Download_PM2
 npm install pm2@latest -g
