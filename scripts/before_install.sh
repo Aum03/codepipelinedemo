f7b4d55e7854ef82dfa4e986972d687f07bde7c3
Aum <aumyadav0311@gmail.com>
2023-10-08 17:41:28 +0530

    modified before install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,9 +6,8 @@ cd /home/ec2-user/server
 #_Update_&_Set_Node_Version
 
 # For Debian/Ubuntu-based systems
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_14.x $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-   
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo -E bash -
+    
 
 
 #_Download_Node_&NPM
